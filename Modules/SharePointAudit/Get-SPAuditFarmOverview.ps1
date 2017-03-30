b6a5d9eb717c5523c1ceb578e1a1734d53c55ea8
evgeni <evgeni.petrov@gmail.com>
2017-03-30 15:33:39 -0700

    Added configuration database to report.

--- Modules/SharePointAudit/Get-SPAuditFarmOverview.ps1
+++ Modules/SharePointAudit/Get-SPAuditFarmOverview.ps1
@@ -3,13 +3,14 @@
   PROCESS {
     $farm = Get-SPFarm
     $date = Get-Date
+    $configurationDatabase = Get-SPDatabase | Where-Object -FilterScript {$_.Type -eq 'Configuration Database'}
     
     $properties = @{
       'ReportName'          = 'Farm Overview'
       'FarmName'            = $farm.Name
       'BuildVersion'        = $farm.BuildVersion.ToString()
       'SharePointEdition'   = ''
-      'ConfigurationDatabase' = ''
+      'ConfigurationDatabase' = $configurationDatabase.Name
       'FarmServerCount'     = $farm.Servers.Count
       'ReportCreatedOn'     = $date.ToString()
     }
