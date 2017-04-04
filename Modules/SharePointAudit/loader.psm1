b9cfb448df01088c6d98e563cae86de018dad8d6
evgeni <evgeni.petrov@gmail.com>
2017-04-04 14:04:58 -0700

    Finished 4.0 Site Topology

--- Modules/SharePointAudit/loader.psm1
+++ Modules/SharePointAudit/loader.psm1
@@ -14,8 +14,9 @@
 . $PSScriptRoot\Get-SPAuditComputerNameByHost.ps1
 . $PSScriptRoot\Get-SPAuditContentDatabases.ps1
 . $PSScriptRoot\Get-SPAuditFarmOverview.ps1
+. $PSScriptRoot\Get-SPAuditFarmTopology.ps1
 . $PSScriptRoot\Get-SPAuditServersInFarm.ps1
 . $PSScriptRoot\Get-SPAuditSharePointBuild.ps1
 . $PSScriptRoot\Get-SPAuditSharePointLicense.ps1
+. $PSScriptRoot\Get-SPAuditSiteTopology.ps1
 . $PSScriptRoot\Get-SPAuditWebApplicationsAndSiteCollections.ps1
-. $PSScriptRoot\Get-SPAuditFarmTopology.ps1
