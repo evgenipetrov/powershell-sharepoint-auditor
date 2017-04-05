88719d60fe28469bf1c59ce28fc3a7b7de700e3e
evgeni <evgeni.petrov@gmail.com>
2017-04-05 08:31:40 -0700

    Finished Services on Server

---
+++ Modules/SharePointAudit/Get-SPAuditServicesOnServer.ps1
@@ -0,0 +1,27 @@
+﻿function Get-SPAuditServicesOnServer
+{
+  $servers = Get-SPServer | Where-Object -FilterScript {
+    $_.Role -eq 'Application'
+  }
+  
+  $services = Get-SPServiceInstance
+  
+  foreach ($service in $services)
+  {
+    $properties = @{
+      'ServiceName' = $service.TypeName
+    }
+    
+    foreach ($server in $servers)
+    {
+      $serverName = $server.Address
+      $serviceStatus = $service.Status
+      
+      $properties.Add($serverName,$serviceStatus)
+    }
+        
+    $output = New-Object -TypeName PSObject -Property $properties
+
+    Write-Output -InputObject $output
+  }
+}
