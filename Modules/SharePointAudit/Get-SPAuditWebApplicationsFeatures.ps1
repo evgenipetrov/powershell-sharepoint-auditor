cec9bbd93f3dd9cced13aabd26d26b5b65199658
evgeni <evgeni.petrov@gmail.com>
2017-04-05 13:41:25 -0700

    Finished Get-SPAuditWebApplicationsFeatures

---
+++ Modules/SharePointAudit/Get-SPAuditWebApplicationsFeatures.ps1
@@ -0,0 +1,27 @@
+﻿function Get-SPAuditWebApplicationsFeatures
+{
+  $features = Get-SPFeature | Where-Object -FilterScript {
+    $_.Scope -eq 'WebApplication'
+  }
+  $webApplications = Get-SPWebApplication
+  
+  foreach ($webApplication in $webApplications)
+  {
+    foreach ($feature in $features)
+    {
+      $title = $feature.GetTitle(1033)
+      $isActive = ($webApplication.Features | Where-Object {$_.DefinitionId -eq $feature.Id}) -ne $null
+      
+      $properties = @{
+        'DisplayName'      = $webApplication.DisplayName
+        'FeatureName'      = $title
+        'CompatibilityLevel' = $feature.CompatibilityLevel
+        'IsActive'         = $isActive
+      }
+    
+      $output = New-Object -TypeName PSObject -Property $properties
+      
+      Write-Output -InputObject $output
+    }
+  }
+}
