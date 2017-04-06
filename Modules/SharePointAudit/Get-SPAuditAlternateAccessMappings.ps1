99602cbcac18c491a655d2064d701982db6d4b09
evgeni <evgeni.petrov@gmail.com>
2017-04-06 11:02:46 -0700

    finished alternate access mappings

---
+++ Modules/SharePointAudit/Get-SPAuditAlternateAccessMappings.ps1
@@ -0,0 +1,23 @@
+﻿function Get-SPAuditAlternateAccessMappings
+{
+  $webApplications = Get-SPWebApplication -IncludeCentralAdministration
+    
+  foreach ($webApplication in $webApplications)
+  {
+    $alternateUrls = Get-SPAlternateURL -WebApplication $webApplication
+    
+    foreach ($alternateUrl in $alternateUrls)
+    {
+      $properties = @{
+        'DisplayName' = $webApplication.DisplayName
+        'InternalUrl' = $alternateUrl.IncomingUrl
+        'Zone' = $alternateUrl.Zone
+        'Url' = $alternateUrl.PublicUrl
+      }
+      
+      $output = New-Object -TypeName PSObject -Property $properties
+      
+      Write-Output $output
+    }
+  }
+}
